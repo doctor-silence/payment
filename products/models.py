2e7c4470ccf676d475051c4a11615f87df31c8d0
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-11-25 16:10:23 +0700

    add_model

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    description = models.TextField(max_length=200, blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
