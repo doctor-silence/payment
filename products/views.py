218d51c3284363823f393d1bf061b972ec6e98b9
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-11-25 18:54:56 +0700

    add_view

---
+++ products/views.py
@@ -0,0 +1,63 @@
+import stripe
+from django.conf import settings
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+from django.views import View
+from .models import Item
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+class SuccessView(TemplateView):
+    template_name = "success.html"
+
+
+class CancelView(TemplateView):
+    template_name = "cancel.html"
+
+
+class ProductHomePageView(TemplateView):
+    model = Item
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        product = Item.objects.get()
+        context = super(ProductHomePageView, self).get_context_data(**kwargs)
+        context.update({
+            "product": product,
+            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
+        })
+        return context
+
+class CreateCheckoutSessionView(View):
+    def get(self, request, *args, **kwargs):
+        product_id = self.kwargs['pk']
+        product = Item.objects.get(id=product_id)
+        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': Item.price,
+                        'product_data': {
+                            'name': Item.name,
+                            'description': Item.description
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            metadata={
+                "product_id": product.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
+
+
+
